c297b06f4923a01a06006b17a668aa87a27cfe26
sushma0918 <sushmamanapuram18@gmail.com>
2025-12-14 10:11:46 +0530

    Add files via upload

---
+++ si.py
@@ -0,0 +1,7 @@
+#24331A05G8
+#To calculate simple interest
+P=int(input("enter the principal amount"))
+T=int(input("enter the  time"))
+R=int(input("enter rate of interest "))
+si=(P*T*R)/100
+print(si)
